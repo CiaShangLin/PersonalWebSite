15ff91fd9f02502de48ea7f4e40247ff0aaabfeb
CaiShangLin <nick034po@yahoo.com.tw>
2024-08-30 17:17:19 +0800

    2024/8/30 1.新增開發日誌頁

--- lib/page/log/LogPage.dart
+++ lib/page/log/LogPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LogPage extends StatefulWidget {
   const LogPage({super.key});
@@ -13,7 +14,12 @@ class _LogPageState extends State<LogPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("123"),
+      child: Column(
+        children: [
+          Text("開發日誌",style: Theme.of(context).textTheme.headlineLarge,),
+          Text("2024/8/30\n暫時先推上Github上\n實作簡易功能跟UI有空再開發",style: Theme.of(context).textTheme.bodyMedium,)
+        ],
+      ),
     );
   }
 }
